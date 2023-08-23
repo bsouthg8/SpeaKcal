01b21271fcf48c1081ff51e0369f72470aa37bc7
James Dunlop <jamezdunlop@gmail.com>
2023-08-23 23:02:19 +1200

    JurnalEntry

---
+++ app/src/main/java/com/example/speakcalapp/JournalEntry.java
@@ -0,0 +1,20 @@
+package com.example.speakcalapp;
+
+public class JournalEntry {
+
+    int entryID;
+    String date;
+    String foodItem;
+    int calories;
+
+    //potentaly will move into another class
+    public void creatEntry(){
+
+    }
+    public void updataEntry(){
+
+    }
+    public void deleteEntry(){
+
+    }
+}
